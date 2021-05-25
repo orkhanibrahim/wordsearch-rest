ff8e56969ed3645a9cd91fe2aaa82aaee7f823b9
Orxan T. Ibrahim <IbrahimOT@kapitalho.az>
2021-05-25 15:28:39 +0400

    custom checkstyle file has been added and optimized unused imports

--- src/main/java/com/orxanibrahim/wordsearchapi/controller/WordSearchController.java
+++ src/main/java/com/orxanibrahim/wordsearchapi/controller/WordSearchController.java
@@ -2,12 +2,11 @@ package com.orxanibrahim.wordsearchapi.controller;
 
 import com.orxanibrahim.wordsearchapi.services.WordGridService;
 import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController("/")
 public class WordSearchController {
@@ -18,13 +17,13 @@ public class WordSearchController {
     @GetMapping("/wordgrid")
     public String createWordGrid(@RequestParam int gridSize, @RequestParam String wordsList) {
         List<String> words = Arrays.asList(wordsList.split("-"));
-        char[][] grid = wordGridService.generateGrid(gridSize,words);
-        String gridToString ="";
+        char[][] grid = wordGridService.generateGrid(gridSize, words);
+        String gridToString = "";
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
                 gridToString += grid[i][j] + " ";
             }
-            gridToString +="\r\n";
+            gridToString += "\r\n";
         }
         return gridToString;
     }
